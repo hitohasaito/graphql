fc1cf89fade4db8492a8f594ab1dc65c114b34b4
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-08-18 16:13:56 +0900

    設定に必要なファイル群を生成し、設定を追加

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,4 +10,9 @@ Types::QueryType = GraphQL::ObjectType.define do
       "Hello World!"
     }
   end
+  field :blog, !Types::BlogType do
+    resolve ->(_obj, _args, ctx) {
+      ctx[:blog]
+    }
+  end
 end
